adc9f9cd113f7e44859fdfee8b724b870ec50c45
Hemachandrika11 <hemapardeep8@gmail.com>
2025-06-03 09:54:40 +0530

    Update menu component HTML, CSS, and TS

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  starters = [
+    { name: 'Paneer Tikka', price: 180 },
+    { name: 'Veg Manchurian', price: 160 },
+    { name: 'Gobi 65', price: 150 },
+    { name: 'Chicken 65', price: 200 },
+    { name: 'Tandoori Chicken', price: 250 },
+  ];
+
+  breads = [
+    { name: 'Plain Naan', price: 40 },
+    { name: 'Butter Naan', price: 50 },
+    { name: 'Garlic Naan', price: 60 },
+    { name: 'Tandoori Roti', price: 30 },
+    { name: 'Lachha Paratha', price: 55 },
+  ];
+
 
 }
