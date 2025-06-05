66130fc1d75a181803da0b3c140e23639ea4e08a
Senthamil143 <senthamil0214@gmail.com>
2025-06-05 21:15:54 +0530

    updated profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // <-- Add this import
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // <-- Add this line
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface User {
+  id: number;
+  name: string;
+  mobile: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: User | null = null;
+  loading = true;
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<User>('http://127.0.0.1:5000/users/2').subscribe({
+      next: (data) => {
+        this.user = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load profile.';
+        this.loading = false;
+      }
+    });
+  }
 }
